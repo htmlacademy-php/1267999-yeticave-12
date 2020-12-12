4152134e02a26aa4e1f7e3c48afc4b52f82b4cf3
Константин <lable_01@mail.ru>
2020-12-12 08:19:36 +0300

    исправил недостатки

--- index.php
+++ index.php
@@ -1,42 +1,49 @@
 <?php
 $is_auth = rand(0, 1);
 $user_name = 'Konstantin'; // укажите здесь ваше имя
-$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+$categories = [
+    'boards' => 'Доски и лыжи',
+    'mounts' => 'Крепления',
+    'boots' => 'Ботинки',
+    'clothes' => 'Одежда',
+    'tools' => 'Инструменты',
+    'various' => 'Разное'
+];
 $ads = [
     [
         'name' => '2014 Rossignol District Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => '10999',
+        'category' => $categories['boards'],
+        'price' => 10999,
         'url' => 'img/lot-1.jpg'
     ],
     [
         'name' => 'DC Ply Mens 2016/2017 Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => '159999',
+        'category' => $categories['boards'],
+        'price' => 159999,
         'url' => 'img/lot-2.jpg'
     ],
     [
         'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'category' => 'Крепления',
-        'price' => '8000',
+        'category' => $categories['mounts'],
+        'price' => 8000,
         'url' => 'img/lot-3.jpg'
     ],
     [
         'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'category' => 'Ботинки',
-        'price' => '10999',
+        'category' => $categories['boots'],
+        'price' => 10999,
         'url' => 'img/lot-4.jpg'
     ],
     [
         'name' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'category' => 'Одежда',
-        'price' => '7500',
+        'category' => $categories['clothes'],
+        'price' => 7500,
         'url' => 'img/lot-5.jpg'
     ],
     [
         'name' => 'Маска Oakley Canopy',
-        'category' => 'Разное',
-        'price' => '5400',
+        'category' => $categories['various'],
+        'price' => 5400,
         'url' => 'img/lot-6.jpg'
     ]
 ];
@@ -48,13 +55,10 @@ $ads = [
  */
 function get_price(int $price): string
 {
-    if ($price > 0 && $price < 1000) {
-        return $price . ' ₽';
-    } elseif ($price >= 1000) {
+    if ($price >= 1000) {
         return number_format($price, 0, '', ' ') . ' ₽';
-    } else {
-        return 'Возникли технические неполадки';
     }
+    return $price . ' ₽';
 }
 ?>
 <!DOCTYPE html>
@@ -84,7 +88,7 @@ function get_price(int $price): string
         <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
             <?php if ($is_auth): ?>
             <div class="user-menu__logged">
-                <p><?=$user_name;?></p>
+                <p><?= $user_name; ?></p>
                 <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                 <a class="user-menu__logout" href="#">Выход</a>
             </div>
@@ -122,15 +126,15 @@ function get_price(int $price): string
             <?php foreach ($ads as $ad): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$ad['url']; ?>" width="350" height="260" alt="<?=$ad['name']; ?>">
+                    <img src="<?= $ad['url']; ?>" width="350" height="260" alt="<?= $ad['name']; ?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$ad['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$ad['name']?></a></h3>
+                    <span class="lot__category"><?= $ad['category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $ad['name']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=get_price($ad['price']); ?></span>
+                            <span class="lot__cost"><?= get_price($ad['price']); ?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -150,7 +154,7 @@ function get_price(int $price): string
             <!--заполните этот список из массива категорий-->
             <?php foreach ($categories as $category): ?>
             <li class="nav__item">
-                <a href="pages/all-lots.html"><?=$category; ?></a>
+                <a href="pages/all-lots.html"><?= $category; ?></a>
             </li>
             <?php endforeach; ?>
         </ul>
