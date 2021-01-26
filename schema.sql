7e5e1e4ee006a33272931fe26bd006c0762d6029
Константин <lable_01@mail.ru>
2021-01-26 22:09:31 +0300

    Пишем SQL-запросы

--- schema.sql
+++ schema.sql
@@ -1,30 +1,69 @@
+DROP DATABASE IF EXISTS yeticave;
 CREATE DATABASE yeticave
     DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
-USE yeticave;
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL UNIQUE,
-    date_registration TIMESTAMP,
-    email VARCHAR(32) NOT NULL UNIQUE,
-    name VARCHAR(32) NOT NULL,
-    password VARCHAR(32) NOT NULL,
-    contacts VARCHAR(128) NOT NULL UNIQUE
+
+CREATE TABLE `yeticave`.`category` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(128) NOT NULL,
+  `cod` VARCHAR(128) NOT NULL,
+  PRIMARY KEY (`id`)
 );
-CREATE TABLE rate (
-    date_rate TIMESTAMP PRIMARY KEY NOT NULL,
-    sum INT NOT NULL
+
+CREATE TABLE `yeticave`.`user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `date_registration` TIMESTAMP NOT NULL,
+  `email` VARCHAR(128) NOT NULL,
+  `name` VARCHAR(128) NOT NULL,
+  `password` VARCHAR(256) NOT NULL,
+  `contacts` VARCHAR(512) NOT NULL,
+  PRIMARY KEY (`id`)
 );
-CREATE TABLE categories (
-    cod INT PRIMARY KEY NOT NULL,
-    category VARCHAR(32) NOT NULL
-);
-CREATE TABLE items (
-    name VARCHAR(32) PRIMARY KEY NOT NULL,
-    creation_date TIMESTAMP NOT NULL,
-    description VARCHAR(256) NOT NULL,
-    image VARCHAR(64) NOT NULL UNIQUE,
-    starting_price INT NOT NULL,
-    completion_date TIMESTAMP NOT NULL,
-    step_rate INT NOT NULL
-);
+
+CREATE TABLE `yeticave`.`lot` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id_category` INT NOT NULL,
+  `id_user_create` INT NOT NULL,
+  `date_creation` TIMESTAMP NOT NULL,
+  `name` VARCHAR(128) NOT NULL,
+  `description` VARCHAR(512) NULL,
+  `image` VARCHAR(256) NULL,
+  `price_starting` DECIMAL(10,0) NOT NULL,
+  `date_completion` TIMESTAMP NOT NULL,
+  `step_rate` DECIMAL(10,0) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_category_lot_idx` (`id_category` ASC),
+  INDEX `fk_user_lot_idx` (`id_user_create` ASC),
+  CONSTRAINT `fk_category_lot`
+    FOREIGN KEY (`id_category`)
+    REFERENCES `yeticave`.`category` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_lot`
+    FOREIGN KEY (`id_user_create`)
+    REFERENCES `yeticave`.`user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION);
+
+CREATE TABLE `yeticave`.`rate` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id_user_game` INT NOT NULL,
+  `id_lot` INT NOT NULL,
+  `date_rate` TIMESTAMP NOT NULL,
+  `price_rate` DECIMAL(10,0) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_user_rate_idx` (`id_user_game` ASC),
+  INDEX `fk_lot_rate_idx` (`id_lot` ASC),
+  CONSTRAINT `fk_user_rate`
+    FOREIGN KEY (`id_user_game`)
+    REFERENCES `yeticave`.`user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_lot_rate`
+    FOREIGN KEY (`id_lot`)
+    REFERENCES `yeticave`.`lot` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION);
+
+
 
